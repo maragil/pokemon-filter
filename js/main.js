f9ee594483aa05027a0675e836f1c51db647fe1a
Maria <araujomaria84@gmail.com>
2023-05-13 12:35:43 +0200

    Add colors

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const pokemonList = document.querySelector('#pokemonList');
 const headerBtn = document.querySelectorAll('.btn-header');
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 500; i++) {
     fetch(URL + i)
         .then((response) => response.json())
         .then(data => showPokemon(data))
